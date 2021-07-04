e984f6ca0b94d021379cf5f355fb48d4b0395e17
zaid9696 <zw283@hotmail.com>
2021-07-04 16:31:30 +0300

    test4

--- components/Layout.js
+++ components/Layout.js
@@ -23,7 +23,6 @@ const Layout = ({children}) => {
       <meta name="keywords" content="Html, Css, Javascript, React, Next JS, Wordpress, Woocommerce, Web Developer, Web Designer" />
       <meta property="og:type" content="website" />
       <meta property="og:description" content="I design & develop websites and I can make a custom Wordpress theme. I am available for hiring" />
-      // <meta property="og:image" content={Img.src}/>
       <meta property='og:site_name' content="Zaid | Freelance Front-end Developer & Web Designer" key='ogsitename' />
       <link rel="icon" type="svg+xml"  href="/favicon.svg"/>
       <link rel="alternate icon"  href="/favicon.svg"/>
