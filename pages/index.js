225f2ec07a9b7503ce5a5e2e62fcec40811991c6
zaid9696 <zw283@hotmail.com>
2021-07-04 16:24:05 +0300

    test2

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Image from 'next/image';
 import styled from 'styled-components';
 import Hero from '../components/Hero';
 import Skills from '../components/Skills';
-import Portfolio from '../components/Portfolio';
+// import Portfolio from '../components/Portfolio';
 import Contact from '../components/Contact';
 // import {main} from '../data/data';
 
