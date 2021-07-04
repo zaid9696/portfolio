c81b0e3dd7dc2a92aff41ffe60abd7a7dc475f84
zaid9696 <zw283@hotmail.com>
2021-07-04 16:14:41 +0300

    fixing Default Image

--- components/Layout.js
+++ components/Layout.js
@@ -3,7 +3,7 @@ import 'normalize.css';
 import GlobalStyles from '../styles/GlobalStyles';
 import Nav from './Nav';
 import Footer from './Footer';
-import Img from '../assets/images/defaultImage.png';
+// import Img from '../assets/images/defaultImage.png';
 
 
 
@@ -23,7 +23,7 @@ const Layout = ({children}) => {
       <meta name="keywords" content="Html, Css, Javascript, React, Next JS, Wordpress, Woocommerce, Web Developer, Web Designer" />
       <meta property="og:type" content="website" />
       <meta property="og:description" content="I design & develop websites and I can make a custom Wordpress theme. I am available for hiring" />
-      <meta property="og:image" content={Img.src}/>
+      // <meta property="og:image" content={Img.src}/>
       <meta property='og:site_name' content="Zaid | Freelance Front-end Developer & Web Designer" key='ogsitename' />
       <link rel="icon" type="svg+xml"  href="/favicon.svg"/>
       <link rel="alternate icon"  href="/favicon.svg"/>
