2ebbf3348144559f75cf63913a3aaaa0835dc5eb
zaid9696 <zw283@hotmail.com>
2021-07-10 15:56:17 +0300

    adding gatsby-ssr

---
+++ gatsby-ssr.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Layout from './src/components/Layout';
+
+
+export function wrapPageElement({element, props}) {
+
+	return <Layout {...props}>{element}</Layout>
+}
